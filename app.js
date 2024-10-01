516147cf0018406039394590d3e80d9d09f72b21
verdant31 <joao.pedro3110@hotmail.com>
2024-09-30 21:08:29 -0300

    trying to make headers dynamically

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import puppeteer from 'puppeteer';
 import exphbs from 'express-handlebars';
 import { fileURLToPath } from 'url';
 import base64 from 'node-base64-image'
+import PDFMerger  from 'pdf-merger-js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,17 +20,13 @@ app.use(express.json());
 app.post('/generate-pdf', async (req, res) => {
   try {
     const { data } = req.body;
-
-    const html = await new Promise((resolve, reject) => {
-      app.render('template', { data }, (err, renderedHtml) => {
-        if (err) reject(err);
-        else resolve(renderedHtml);
-      });
-    });
-
-    const browser = await puppeteer.launch({headless: false,});
-    const page = await browser.newPage();
-    await page.setContent(html, { waitUntil: 'networkidle2' });
+    // Juntar os conteudos com base na informação de cada header
+    const contents = req.body.headers.map(header => {
+      return {
+        headerContent: header,
+        tableContent: req.body.data.filter(item => item.header === header.key)
+      }
+    })
 
     const logo = 'data:image/png;base64,' + await base64.encode('https://sooro.com.br/wp-content/uploads/2020/09/sooro-by-the-way.png', {
       string: true,
@@ -38,40 +35,55 @@ app.post('/generate-pdf', async (req, res) => {
       }
     });
 
-    const pdf = await page.pdf({
-      format: 'A4',
-      path: 'relatorio.pdf',
-      printBackground: true,
-      displayHeaderFooter: true,
-      margin: { top: '150px', bottom: '200px' },
-      headerTemplate: `
-      <style>
-      html,body,div{-webkit-print-color-adjust: exact}
-      span.logo { height: 80px; width:200px; content: url("${logo}")}
-      </style>
-      <header style="font-size:14px;width:100%; background:#d3540b;height:95px; display:flex; align-items:center">
-          <span class="logo"></span>
-          <h1>UM MONTE DE INFORMAÇÃO LALALLALALALALAL</h1>
-      </header>
-      `,
-      footerTemplate:`
-      <style>
-      span.logo { height: 50px; width:200px; content: url("${logo}")}
-      </style>
-      <footer style="font-size:6px;width:100%; background:green;height:150px;flex-direction:column; display:flex; align-items:center">
-          <span class="logo"></span>
-          <h1>UM MONTE DE INFORMAÇÃO LALALLALALALALAL</h1>
-          <h1>UM MONTE DE INFORMAÇÃO LALALLALALALALAL</h1>
-          <h1>UM MONTE DE INFORMAÇÃO LALALLALALALALAL</h1>
-      </footer>
-      `,
-    });
+    const browser = await puppeteer.launch();
+    const merger = new PDFMerger();
 
-    await browser.close();
+    for (const content of contents) {
+      const html = await new Promise((resolve, reject) => {
+        app.render('template', { data: content.tableContent }, (err, renderedHtml) => {
+          if (err) reject(err);
+          else resolve(renderedHtml);
+        });
+      });
+      const page = await browser.newPage();
+
+
+      await page.setContent(html, { waitUntil: 'networkidle2' });    
+      
+      const headerHtml = await new Promise((resolve, reject) => {
+        app.render('header', { data: content.headerContent }, (err, renderedHtml) => {
+          if (err) reject(err);
+          else resolve(renderedHtml);
+        });
+      });
+
+      await merger.add(await page.pdf({
+        format: 'A4',
+        path: 'relatorio.pdf',
+        printBackground: true,
+        displayHeaderFooter: true,
+        margin: { top: '150px', bottom: '200px' },
+        headerTemplate: headerHtml,
+        footerTemplate:`
+        <style>
+        span.logo { height: 50px; width:200px; content: url("${logo}")}
+        </style>
+        <footer style="font-size:6px;width:100%; background:green;height:150px;flex-direction:column; display:flex; align-items:center">
+            <span class="logo"></span>
+            <h1>UM MONTE DE INFORMAÇÃO LALALLALALALALAL</h1>
+            <h1>UM MONTE DE INFORMAÇÃO LALALLALALALALAL</h1>
+            <h1>UM MONTE DE INFORMAÇÃO LALALLALALALALAL</h1>
+        </footer>
+        `,
+      }));
+    }
+
+    await merger.save("relatorio.pdf");
+
+    await browser.close()
 
     res.contentType('application/pdf');
     res.setHeader('Content-Disposition', 'attachment;filename=generated.pdf');
-    res.send(pdf);
   } catch (error) {
     console.log(error); 
     res.status(500).send('Erro ao gerar o PDF');
